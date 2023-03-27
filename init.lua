865cd9da8593875145b2b57dc88d19740038df3a
ThePython <nolanjbowers@gmail.com>
2023-03-27 15:57:39 -0700

    Just work... This was supposed to take 5 minutes.

--- init.lua
+++ init.lua
@@ -7,10 +7,10 @@ local itemslot_bg = mcl_formspec.get_itemslot_bg
 minetest.register_tool("portable_crafting_table:portable_crafting_table", {
     description = "Portable Crafting Table",
     inventory_image = "crafting_workbench_top.png",
-    on_place = function(itemstack, placer, pointed_thing)
+    on_place = function(itemstack, player, pointed_thing)
         mcl_crafting_table.show_crafting_form(player)
     end,
-    on_secondary_use = function(itemstack, placer, pointed_thing)
+    on_secondary_use = function(itemstack, player, pointed_thing)
         mcl_crafting_table.show_crafting_form(player)
     end
 })
