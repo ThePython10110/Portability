a7c1a328ead74a5468cfa12f817da2113025ae13
ThePython <nolanjbowers@gmail.com>
2023-03-27 15:46:46 -0700

    Should work this time.

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 minetest.register_tool("portable_crafting_table:portable_crafting_table", {
     description = "Portable Crafting Table",
     inventory_image = "crafting_workbench_top.png",
-    on_place = mcl_crafting_table.show_crafting_form(player)
+    on_place = function(pos, node, player, itemstack)
+        mcl_crafting_table.show_crafting_form(player)
+    end
 })
