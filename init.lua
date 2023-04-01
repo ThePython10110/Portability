4ea345ae17569ac65d1aeb953ca8430905bed402
ThePython <nolanjbowers@gmail.com>
2023-04-01 10:18:05 -0700

    Will redo `inventorybags` instead of making chests Might fork [inventorybags](https://github.com/cx384/inventorybags) and use that instead of making my own thing.

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ local modpath = minetest.get_modpath("portability")
 dofile(modpath.."/crafting_table.lua")
 dofile(modpath.."/ender_chest.lua")
 dofile(modpath.."/enchanting_table.lua")
---dofile(modpath.."/chest.lua") --Uncomment for errors!
